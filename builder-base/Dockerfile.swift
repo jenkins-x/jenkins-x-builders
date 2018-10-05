9b2a6a0c71205dde861e993164c321afcba092e1
Gareth Evans <gareth@bryncynfelin.co.uk>
2018-10-05 18:29:36 +0100

    Updated ruby and swift builders

--- builder-base/Dockerfile.swift
+++ builder-base/Dockerfile.swift
@@ -27,15 +27,30 @@ RUN curl -f https://download.docker.com/linux/static/stable/x86_64/docker-$DOCKE
   rm -rf docker
 
 # helm
-ENV HELM_VERSION 2.8.2
+ENV HELM_VERSION 2.10.0
 RUN curl -f https://storage.googleapis.com/kubernetes-helm/helm-v${HELM_VERSION}-linux-amd64.tar.gz  | tar xzv && \
   mv linux-amd64/helm /usr/bin/ && \
   rm -rf linux-amd64
 
+# tiller - using a hacked version until 2.11.x
+RUN curl -f -L https://github.com/jstrachan/helm-tiller-release/releases/download/2.10.0/tiller.tar.gz -o tiller.tgz && \
+  tar xf tiller.tgz && \
+  chmod +x tiller && \
+  mv tiller /usr/bin/
+
 # helm3
-RUN curl -f https://storage.googleapis.com/kubernetes-helm/helm-dev-v3-linux-amd64.tar.gz | tar xzv && \
-  mv linux-amd64/helm /usr/bin/helm3 && \
-  rm -rf linux-amd64
+# RUN curl -L https://storage.googleapis.com/kubernetes-helm/helm-dev-v3-linux-amd64.tar.gz | tar xzv && \
+#  mv linux-amd64/helm /usr/bin/helm3 && \
+#  rm -rf linux-amd64
+
+# lets use a patched release until this PR is merged or helm3 works again ;)
+# https://github.com/kubernetes/helm/pull/4257#issuecomment-399491118
+
+RUN curl -f -L https://github.com/jstrachan/helm/releases/download/untagged-93375777c6644a452a64/helm-linux-amd64.tar.gz -o helm3.tgz && \
+  tar xf helm3.tgz && \
+  mv helm /usr/bin/helm3
+
+RUN rm *.tgz
 
 # gcloud
 ENV GCLOUD_VERSION 187.0.0
@@ -43,10 +58,13 @@ RUN curl -f -L https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google
   mv google-cloud-sdk /usr/bin/
 ENV PATH=$PATH:/usr/bin/google-cloud-sdk/bin
 
+# install the docker credential plugin
+RUN gcloud components install docker-credential-gcr
+
 # jx-release-version
-ENV JX_RELEASE_VERSION 1.0.9
+ENV JX_RELEASE_VERSION 1.0.10
 RUN curl -f -o ./jx-release-version -L https://github.com/jenkins-x/jx-release-version/releases/download/v${JX_RELEASE_VERSION}/jx-release-version-linux && \
-  mv jx-reelease-version /usr/bin/ && \
+  mv jx-release-version /usr/bin/ && \
   chmod +x /usr/bin/jx-release-version
 
 # exposecontroller
@@ -56,21 +74,17 @@ RUN curl -f -L https://github.com/fabric8io/exposecontroller/releases/download/v
   mv exposecontroller /usr/bin/
 
 # skaffold
-ENV SKAFFOLD_VERSION 0.4.0
-# RUN curl -Lo skaffold https://github.com/GoogleCloudPlatform/skaffold/releases/download/v${SKAFFOLD_VERSION}/skaffold-linux-amd64 && \
-# TODO use temp fix distro
-
-RUN curl -f -Lo skaffold https://github.com/jstrachan/skaffold/releases/download/v0.5.0.1-jx2/skaffold-linux-amd64 && \
-chmod +x skaffold && \
+ENV SKAFFOLD_VERSION 0.11.0
+RUN curl -f -Lo skaffold https://github.com/GoogleCloudPlatform/skaffold/releases/download/v${SKAFFOLD_VERSION}/skaffold-linux-amd64 && \
+  chmod +x skaffold && \
   mv skaffold /usr/bin
 
 # updatebot
 ENV UPDATEBOT_VERSION 1.1.26
-RUN curl -f -o ./updatebot -L https://oss.sonatype.org/content/groups/public/io/jenkins/updatebot/updatebot/${UPDATEBOT_VERSION}/updatebot-${UPDATEBOT_VERSION}.jar && \
+RUN curl -f  -o ./updatebot -L https://oss.sonatype.org/content/groups/public/io/jenkins/updatebot/updatebot/${UPDATEBOT_VERSION}/updatebot-${UPDATEBOT_VERSION}.jar && \
   chmod +x updatebot && \
   cp updatebot /usr/bin/ && \
   rm -rf updatebot
-
 
 # draft
 RUN curl -f https://azuredraft.blob.core.windows.net/draft/draft-canary-linux-amd64.tar.gz  | tar xzv && \
@@ -84,8 +98,28 @@ RUN curl -f -LO https://storage.googleapis.com/kubernetes-release/release/$(curl
 
 # jx
 ENV JX_VERSION 1.3.375
-RUN curl -L https://github.com/jenkins-x/jx/releases/download/v${JX_VERSION}/jx-linux-amd64.tar.gz | tar xzv && \
+RUN curl -f -L https://github.com/jenkins-x/jx/releases/download/v${JX_VERSION}/jx-linux-amd64.tar.gz | tar xzv && \
   mv jx /usr/bin/
+
+# aws ecr docker credential helper.
+# Currently using https://github.com/estahn/amazon-ecr-credential-helper as there are no releases yet in the main repo
+# Main repo issues tracking at https://github.com/awslabs/amazon-ecr-credential-helper/issues/80
+RUN mkdir ecr && \
+    curl -f -L https://github.com/estahn/amazon-ecr-credential-helper/releases/download/v0.1.1/amazon-ecr-credential-helper_0.1.1_linux_amd64.tar.gz | tar -xzv -C ./ecr/ && \
+    mv ecr/docker-credential-ecr-login /usr/bin/ && \
+    rm -rf ecr
+
+# ACR docker credential helper
+#??https://github.com/Azure/acr-docker-credential-helper
+RUN mkdir acr && \
+    curl -f -L https://aadacr.blob.core.windows.net/acr-docker-credential-helper/docker-credential-acr-linux-amd64.tar.gz | tar -xzv -C ./acr/ && \
+    mv acr/docker-credential-acr-linux /usr/bin/ && \
+    rm -rf acr
+    
+# reflex
+ENV REFLEX_VERSION 0.3.1
+RUN curl -f -L https://github.com/ccojocar/reflex/releases/download/v${REFLEX_VERSION}/reflex_${REFLEX_VERSION}_linux_amd64.tar.gz | tar xzv && \
+  mv reflex /usr/bin/
 
 ENV PATH ${PATH}:/opt/google/chrome
 
