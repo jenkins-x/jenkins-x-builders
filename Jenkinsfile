50ac5f93cf4c65827debbbbf100d4acefa0405db
Gareth Evans <gareth@bryncynfelin.co.uk>
2018-10-05 09:42:20 +0100

    Initial commit of builder-base

---
+++ Jenkinsfile
@@ -0,0 +1,43 @@
+pipeline {
+    agent any
+    environment {
+      ORG               = 'jenkins-x'
+      DOCKER_ORG        = 'jenkinsxio'
+      APP_NAME          = 'jenkins-x-builders'
+      GIT_PROVIDER      = 'github.com'
+    }
+    stages {
+      stage('CI Build and push snapshot') {
+        when {
+          branch 'PR-*'
+        }
+        environment {
+          PREVIEW_VERSION = "0.0.0-SNAPSHOT-$BRANCH_NAME-$BUILD_NUMBER"
+          PREVIEW_NAMESPACE = "$APP_NAME-$BRANCH_NAME".toLowerCase()
+          HELM_RELEASE = "$PREVIEW_NAMESPACE".toLowerCase()
+        }
+        steps {
+          checkout scm
+          sh 'export VERSION=$PREVIEW_VERSION'
+          sh "make build"
+          sh './jx-docker-build.sh $PREVIEW_VERSION $DOCKER_ORG pr'
+        }
+      }
+      stage('Build Release') {
+        when {
+          branch 'master'
+        }
+        steps {
+          git 'https://github.com/jenkins-x/jenkins-x-builders.git'
+          sh "git config --global credential.helper store"
+          sh "jx step validate --min-jx-version 1.1.73"
+          sh "jx step git credentials"
+          sh "echo \$(jx-release-version) > VERSION"
+          sh "make build"
+          sh 'export VERSION=`cat VERSION`'
+          sh "jx step validate --min-jx-version 1.2.36"
+          sh './jx-docker-build.sh `cat VERSION` $DOCKER_ORG release'
+        }
+      }
+    }
+  }
---
+++ builder-base/Dockerfile.swift
@@ -0,0 +1,92 @@
+FROM swift:4.0.3
+
+RUN apt-get update && apt-get install -y \
+  wget \
+  bzip2 \
+  python-pip
+
+RUN pip install --upgrade pip anchorecli
+
+# java required for updatebot
+RUN apt-get update && apt-get install -y openjdk-8-jre
+
+# chrome
+RUN apt-get install -y libappindicator1 fonts-liberation libasound2 libnspr4 libnss3 libxss1 lsb-release xdg-utils libappindicator3-1 && \
+    wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb && \
+    dpkg -i google-chrome*.deb && \
+    rm google-chrome*.deb
+ 
+
+# USER jenkins
+WORKDIR /home/jenkins
+
+# Docker
+ENV DOCKER_VERSION 17.12.0
+RUN curl -f https://download.docker.com/linux/static/stable/x86_64/docker-$DOCKER_VERSION-ce.tgz | tar xvz && \
+  mv docker/docker /usr/bin/ && \
+  rm -rf docker
+
+# helm
+ENV HELM_VERSION 2.8.2
+RUN curl -f https://storage.googleapis.com/kubernetes-helm/helm-v${HELM_VERSION}-linux-amd64.tar.gz  | tar xzv && \
+  mv linux-amd64/helm /usr/bin/ && \
+  rm -rf linux-amd64
+
+# helm3
+RUN curl -f https://storage.googleapis.com/kubernetes-helm/helm-dev-v3-linux-amd64.tar.gz | tar xzv && \
+  mv linux-amd64/helm /usr/bin/helm3 && \
+  rm -rf linux-amd64
+
+# gcloud
+ENV GCLOUD_VERSION 187.0.0
+RUN curl -f -L https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-${GCLOUD_VERSION}-linux-x86_64.tar.gz | tar xzv && \
+  mv google-cloud-sdk /usr/bin/
+ENV PATH=$PATH:/usr/bin/google-cloud-sdk/bin
+
+# jx-release-version
+ENV JX_RELEASE_VERSION 1.0.9
+RUN curl -f -o ./jx-release-version -L https://github.com/jenkins-x/jx-release-version/releases/download/v${JX_RELEASE_VERSION}/jx-release-version-linux && \
+  mv jx-reelease-version /usr/bin/ && \
+  chmod +x /usr/bin/jx-release-version
+
+# exposecontroller
+ENV EXPOSECONTROLLER_VERSION 2.3.34
+RUN curl -f -L https://github.com/fabric8io/exposecontroller/releases/download/v$EXPOSECONTROLLER_VERSION/exposecontroller-linux-amd64 > exposecontroller && \
+  chmod +x exposecontroller && \
+  mv exposecontroller /usr/bin/
+
+# skaffold
+ENV SKAFFOLD_VERSION 0.4.0
+# RUN curl -Lo skaffold https://github.com/GoogleCloudPlatform/skaffold/releases/download/v${SKAFFOLD_VERSION}/skaffold-linux-amd64 && \
+# TODO use temp fix distro
+
+RUN curl -f -Lo skaffold https://github.com/jstrachan/skaffold/releases/download/v0.5.0.1-jx2/skaffold-linux-amd64 && \
+chmod +x skaffold && \
+  mv skaffold /usr/bin
+
+# updatebot
+ENV UPDATEBOT_VERSION 1.1.26
+RUN curl -f -o ./updatebot -L https://oss.sonatype.org/content/groups/public/io/jenkins/updatebot/updatebot/${UPDATEBOT_VERSION}/updatebot-${UPDATEBOT_VERSION}.jar && \
+  chmod +x updatebot && \
+  cp updatebot /usr/bin/ && \
+  rm -rf updatebot
+
+
+# draft
+RUN curl -f https://azuredraft.blob.core.windows.net/draft/draft-canary-linux-amd64.tar.gz  | tar xzv && \
+  mv linux-amd64/draft /usr/bin/ && \
+  rm -rf linux-amd64
+
+# kubectl
+RUN curl -f -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl && \
+  chmod +x kubectl && \
+  mv kubectl /usr/bin/
+
+# jx
+ENV JX_VERSION 1.3.375
+RUN curl -L https://github.com/jenkins-x/jx/releases/download/v${JX_VERSION}/jx-linux-amd64.tar.gz | tar xzv && \
+  mv jx /usr/bin/
+
+ENV PATH ${PATH}:/opt/google/chrome
+
+CMD ["helm","version"]
