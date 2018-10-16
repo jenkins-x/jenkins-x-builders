829f5fa1763cd57b3e5b356fb96b04425d11032c
Gareth Evans <gareth@bryncynfelin.co.uk>
2018-10-16 17:32:55 +0100

    fix: reordered docker push commands to avoid prune issue

--- jx-docker-build.sh
+++ jx-docker-build.sh
@@ -29,13 +29,11 @@ BROKEN="dotnet"
 ## now loop through the above array
 for i in $BUILDERS
 do
-  echo "building builder-${i}"
+  echo "updating builder-${i}"
   pushd builder-${i}
     sed -i.bak -e "s/FROM \(.*\)\/builder-\(.*\):\(.*\)/FROM ${DOCKER_ORG}\/builder-\2:${VERSION}/" Dockerfile
     rm Dockerfile.bak
     head -n 1 Dockerfile
-    echo "Building ${DOCKER_REGISTRY}/${DOCKER_ORG}/builder-${i}:${VERSION}"
-    docker build ${CACHE} -t ${DOCKER_REGISTRY}/${DOCKER_ORG}/builder-${i}:${VERSION} -f Dockerfile .
   popd
 done
 
@@ -43,22 +41,30 @@ if [ "release" == "${RELEASE}" ]; then
   jx step tag --version ${VERSION}
 fi
 
-for i in ${BUILDERS}
+for i in $BUILDERS
 do
-  if [ "${PUSH}" = "true" ]; then
-    echo "Pushing builder-${i}:${VERSION} to ${DOCKER_REGISTRY}"
-    docker push ${DOCKER_REGISTRY}/${DOCKER_ORG}/builder-${i}:${VERSION}
+  echo "building builder-${i}"
+  pushd builder-${i}
+    head -n 1 Dockerfile
+    echo "Building ${DOCKER_REGISTRY}/${DOCKER_ORG}/builder-${i}:${VERSION}"
+    docker build ${CACHE} -t ${DOCKER_REGISTRY}/${DOCKER_ORG}/builder-${i}:${VERSION} -f Dockerfile .
 
-    if [ "${PUSH_LATEST}" = "true" ]; then
-      echo "Pushing builder-${i}:latest to ${DOCKER_REGISTRY}"
-      docker tag ${DOCKER_REGISTRY}/${DOCKER_ORG}/builder-${i}:${VERSION} ${DOCKER_REGISTRY}/${DOCKER_ORG}/builder-${i}:latest
-      docker push ${DOCKER_REGISTRY}/${DOCKER_ORG}/builder-${i}:latest
+    if [ "${PUSH}" = "true" ]; then
+      echo "Pushing builder-${i}:${VERSION} to ${DOCKER_REGISTRY}"
+      docker push ${DOCKER_REGISTRY}/${DOCKER_ORG}/builder-${i}:${VERSION}
+
+      if [ "${PUSH_LATEST}" = "true" ]; then
+        echo "Pushing builder-${i}:latest to ${DOCKER_REGISTRY}"
+        docker tag ${DOCKER_REGISTRY}/${DOCKER_ORG}/builder-${i}:${VERSION} ${DOCKER_REGISTRY}/${DOCKER_ORG}/builder-${i}:latest
+        docker push ${DOCKER_REGISTRY}/${DOCKER_ORG}/builder-${i}:latest
+      else
+        echo "Not pushing the latest docker image as PUSH_LATEST=${PUSH_LATEST}"
+      fi
     else
-      echo "Not pushing the latest docker image as PUSH_LATEST=${PUSH_LATEST}"
+      echo "Not pushing the docker image as PUSH=${PUSH}"
     fi
-  else
-    echo "Not pushing the docker image as PUSH=${PUSH}"
-  fi
+
+  popd
 done
 
 if [ "release" == "${RELEASE}" ]; then
