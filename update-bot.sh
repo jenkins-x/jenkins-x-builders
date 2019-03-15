4bb1a6dc5358bc08950f50beec904e16e8540019
James Strachan <james.strachan@gmail.com>
2019-03-15 10:05:42 +0000

    fix updatebot script for GCR images

--- update-bot.sh
+++ update-bot.sh
@@ -5,30 +5,30 @@ set -o nounset
 set -o pipefail
 
 updatebot push-version --kind helm \
-    jenkinsxio/builder-aws-cdk ${VERSION} \
-    jenkinsxio/builder-ruby ${VERSION} \
-    jenkinsxio/builder-swift ${VERSION} \
-    jenkinsxio/builder-dlang ${VERSION} \
-    jenkinsxio/builder-go ${VERSION} \
-    jenkinsxio/builder-go-maven ${VERSION} \
-    jenkinsxio/builder-gradle ${VERSION} \
-    jenkinsxio/builder-gradle4 ${VERSION} \
-    jenkinsxio/builder-gradle5 ${VERSION} \
-    jenkinsxio/builder-jx ${VERSION} \
-    jenkinsxio/builder-maven ${VERSION} \
-    jenkinsxio/builder-maven-32 ${VERSION} \
-    jenkinsxio/builder-maven-java11 ${VERSION} \
-    jenkinsxio/builder-maven-nodejs ${VERSION} \
-    jenkinsxio/builder-newman ${VERSION} \
-    jenkinsxio/builder-nodejs ${VERSION} \
-    jenkinsxio/builder-nodejs8x ${VERSION} \
-    jenkinsxio/builder-nodejs10x ${VERSION} \
-    jenkinsxio/builder-python ${VERSION} \
-    jenkinsxio/builder-python2 ${VERSION} \
-    jenkinsxio/builder-python37 ${VERSION} \
-    jenkinsxio/builder-rust ${VERSION} \
-    jenkinsxio/builder-scala ${VERSION} \
-    jenkinsxio/builder-terraform ${VERSION}
+    gcr.io/jenkinsxio/builder-aws-cdk ${VERSION} \
+    gcr.io/jenkinsxio/builder-ruby ${VERSION} \
+    gcr.io/jenkinsxio/builder-swift ${VERSION} \
+    gcr.io/jenkinsxio/builder-dlang ${VERSION} \
+    gcr.io/jenkinsxio/builder-go ${VERSION} \
+    gcr.io/jenkinsxio/builder-go-maven ${VERSION} \
+    gcr.io/jenkinsxio/builder-gradle ${VERSION} \
+    gcr.io/jenkinsxio/builder-gradle4 ${VERSION} \
+    gcr.io/jenkinsxio/builder-gradle5 ${VERSION} \
+    gcr.io/jenkinsxio/builder-jx ${VERSION} \
+    gcr.io/jenkinsxio/builder-maven ${VERSION} \
+    gcr.io/jenkinsxio/builder-maven-32 ${VERSION} \
+    gcr.io/jenkinsxio/builder-maven-java11 ${VERSION} \
+    gcr.io/jenkinsxio/builder-maven-nodejs ${VERSION} \
+    gcr.io/jenkinsxio/builder-newman ${VERSION} \
+    gcr.io/jenkinsxio/builder-nodejs ${VERSION} \
+    gcr.io/jenkinsxio/builder-nodejs8x ${VERSION} \
+    gcr.io/jenkinsxio/builder-nodejs10x ${VERSION} \
+    gcr.io/jenkinsxio/builder-python ${VERSION} \
+    gcr.io/jenkinsxio/builder-python2 ${VERSION} \
+    gcr.io/jenkinsxio/builder-python37 ${VERSION} \
+    gcr.io/jenkinsxio/builder-rust ${VERSION} \
+    gcr.io/jenkinsxio/builder-scala ${VERSION} \
+    gcr.io/jenkinsxio/builder-terraform ${VERSION}
 updatebot push-regex -r "builderTag: (.*)" -v ${VERSION} jx-build-templates/values.yaml
-updatebot push-regex -r "\s+tag: (.*)" -v ${VERSION} --previous-line "\s+repository: jenkinsxio/builder-go" values.yaml
-updatebot push-regex -r "\s+tag: (.*)" -v ${VERSION} --previous-line "\s+repository: jenkinsxio/builder-maven" prow/values.yaml
+updatebot push-regex -r "\s+tag: (.*)" -v ${VERSION} --previous-line "\s+repository: gcr.io/jenkinsxio/builder-go" values.yaml
+updatebot push-regex -r "\s+tag: (.*)" -v ${VERSION} --previous-line "\s+repository: gcr.io/jenkinsxio/builder-maven" prow/values.yaml
