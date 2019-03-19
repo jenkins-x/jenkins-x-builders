8857e7ea3ad0c825b4349f853e9369e0386be84c
Gareth Evans <gareth@bryncynfelin.co.uk>
2019-03-19 14:26:48 +0000

    fix: correcting replace versions script

--- replace-versions.sh
+++ replace-versions.sh
@@ -17,7 +17,7 @@ set -o nounset
 set -o pipefail
 
 
-BUILDERS="aws-cdk awscli awscli-local go-maven maven-nodejs"
+BUILDERS="go-maven maven-nodejs"
 
 ## now loop through the above array
 for i in $BUILDERS
